f216ac349229c576bd4f358e68cf236a01a1cd94
Volodymyr Lipunov <volo_lipu@yahoo.com>
2016-11-30 10:29:15 -0800

    trying

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
 
+import App from './components/app';
 import reducers from './reducers';
 
-import App from './components/App';
-
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
-import TwitterReducer from './TwitterReducer';
 
 const rootReducer = combineReducers({
-  twitter: TwitterReducer
+  state: (state = {}) => state
 });
 
 export default rootReducer;
