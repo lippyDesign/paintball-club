e9cbcafaf93c386b1b939e410bd996262585668f
Volodymyr Lipunov <volo_lipu@yahoo.com>
2016-11-30 10:41:31 -0800

    rrr

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+    return <header className='site-wrapper header'>
+        <div className="header-info">
+            <h1>THE GAME</h1>
+            <h2>Пейнтбольный Клуб</h2>
+            <h3>г. Кондопога</h3>
+        </div>
+    </header>
+}
---
+++ src/containers/interestForm/InterestForm.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class InterestForm extends Component {
+
+    sendEmail(e) {
+        e.preventDefault();
+
+        axios.post('/contactus', {
+            name: this.refs.name.value.trim(),
+            email: this.refs.email.value.trim(),
+            date: this.refs.date.value.trim(),
+            phone: this.refs.phone.value.trim(),
+            numPlayers: this.refs.numPlayers.value.trim(),
+            comments: this.refs.comments.value.trim()
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+        this.refs.name.value = '';
+        this.refs.email.value = '';
+        this.refs.date.value = '';
+        this.refs.phone.value = '';
+        this.refs.numPlayers.value = '';
+        this.refs.comments.value = '';
+    }
+
+    render() {
+        return <form onSubmit={this.sendEmail.bind(this)}>
+            <input type="text"  ref="name" placeholder="Имя и Фамилия" />
+            <input type="tel"  ref="phone" placeholder="Номер Tелефона" />
+            <input type="email"  ref="email" placeholder="Адрес Электронной Почты"/>
+            <input type="date"  ref="date" placeholder="Дата Мероприятия"/>
+            <input type="num"  ref="numPlayers" placeholder="Количество Игроков"/>
+            <textarea ref="comments" placeholder="Комментарии"></textarea>
+            <button><i className="fa fa-envelope"></i> Отправить</button>
+        </form>
+    }
+}
